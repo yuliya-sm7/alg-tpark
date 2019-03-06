f1521f0dae6fd817ff17eae71c86e8a2f019256b
yu <yuliya.sm7@gmail.com>
2019-03-06 20:16:10 +0300

    add comment

--- ADZ_1_3/main.cpp
+++ ADZ_1_3/main.cpp
@@ -1,20 +1,25 @@
+// ID 19528537
+// 1_3. Даны два строго возрастающих массива целых чисел A[0..n) и B[0..m) и число k.
+// Найти количество таких пар индексов (i, j), что A[i] + B[j] = k. Время работы O(n + m).
+// n, m ≤ 100000.
+
 #include <iostream>
 
 size_t input_arr(int **array);
-size_t sums_equals_to(const int* series1, const int* series2, size_t len1, size_t len2, size_t k);
 
+size_t sums_equals_to(const int *series1, const int *series2, \
+                        size_t len1, size_t len2, size_t k);
 
 int main() {
-
-    // заполнение первого массива
-    int* series1;
+    // ввод двух строго возрастающих массивов
+    int *series1;
     size_t len1 = input_arr(&series1);
-    // заполнение второго массива
-    int* series2;
+    int *series2;
     size_t len2 = input_arr(&series2);
 
-    size_t k = 0;
+    size_t k = 0;  // чему равна сумма двух элементов
     std::cin >> k;
+    // вывод колличества пар подходящих индексов
     std::cout << sums_equals_to(series1, series2, len1, len2, k) << std::endl;
 
     delete[] series1;
@@ -26,7 +31,7 @@ int main() {
 size_t input_arr(int **array) {
     size_t len = 0;
     std::cin >> len;
-    int* series = new int[len];
+    int *series = new int[len];
     for (size_t i = 0; i < len; i++) {
         std::cin >> series[i];
     }
@@ -34,19 +39,21 @@ size_t input_arr(int **array) {
     return len;
 }
 
-size_t sums_equals_to(const int* series1, const int* series2, size_t len1, size_t len2, size_t k) {
+size_t sums_equals_to(const int *series1, const int *series2, \
+                        size_t len1, size_t len2, size_t k) {
     size_t count_equals = 0;
-    size_t j_max = len2 - 1;
+    ssize_t j_max = len2 - 1;  // инекс правой границы второго массива
 
+    // проход второго массива по частям
     for (size_t i = 0; i < len1; ++i) {
-        for (int j = j_max; j > -1; --j) {
+        for (ssize_t j = j_max; j > -1; --j) {
             int res = series1[i] + series2[j];
             if (res == k) {
                 count_equals++;
                 j_max = j - 1;
-                break; // конец итерации
+                break;
             } else if (res < k) {
-                break; // конец итерации
+                break;
             }
         }
     }
