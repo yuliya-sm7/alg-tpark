4f9535e0bc7a3834ddb3ddbb73c9f8b28cb22032
yu <yuliya.sm7@gmail.com>
2019-03-16 00:30:58 +0300

    OK

--- DZ_5_1/main.cpp
+++ DZ_5_1/main.cpp
@@ -11,54 +11,86 @@
 #include <iostream>
 #include <string.h>
 
-void Merge(int *a, int a_len, int *b, int b_len, int *c) {
-    int i = 0;
-    int j = 0;
-    for (int k = 0; k < (a_len + b_len); k++) {
-        if (i < a_len && j < b_len) {
-            if (a[i] < b[j]) {
-                c[k] = a[i];
-                i++;
-            } else {
-                c[k] = b[j];
-                j++;
-            }
-        } else if (i >= a_len) {
-            c[k] = b[j];
-            j++;
-        } else {
+
+struct Customer {
+public:
+    int enter;
+    int exit;
+    int ads_count;
+
+    Customer() : enter(0), exit(0), ads_count(0) {};
+
+    bool operator>(const Customer &R) {
+        if (this->exit > R.exit)
+            return true;
+        if (this->exit == R.exit)
+            return this->enter < R.enter;
+        return false;
+    }
+
+    bool operator<=(const Customer &R) {
+        return !(*this > R);
+    }
+
+};
+
+void show(Customer *arr, size_t n, int start, int adTime) {
+    for (int i = start; i < n; i++)
+        if (arr[i].enter <= adTime)
+            arr[i].ads_count++;
+}
+
+template<class T>
+void Merge(T a[], size_t a_len, T b[], size_t b_len, T c[]) {
+    size_t i = 0;
+    size_t j = 0;
+    for (size_t k = 0; k < (a_len + b_len); k++) {
+        if ((j >= b_len) || (i < a_len && a[i] <= b[j])) {
             c[k] = a[i];
             i++;
+        } else {
+            c[k] = b[j];
+            j++;
         }
     }
 }
 
-void MergeSort(int *a, int aLen) {
+template<class T>
+void MergeSort(T a[], size_t aLen) {
     if (aLen <= 1) {
         return;
     }
-    int firstLen = aLen / 2;
-    int secondLen = aLen - firstLen;
+    size_t firstLen = aLen / 2;
+    size_t secondLen = aLen - firstLen;
     MergeSort(a, firstLen);
     MergeSort(a + firstLen, secondLen);
-    int *c = new int[aLen];
+    T *c = new T[aLen];
     Merge(a, firstLen, a + firstLen, secondLen, c);
-    memcpy(a, c, sizeof(int) * aLen);
+    memcpy(a, c, sizeof(T) * aLen);
     delete[] c;
 }
 
 
 int main() {
-    int n = 0;
+    size_t n = 0;
     std::cin >> n;
-    int *A = new int[n];
+    Customer A[n];
     for (int i = 0; i < n; i++) {
-        std::cin >> A[i];
+        std::cin >> A[i].enter >> A[i].exit;
     }
     MergeSort(A, n);
+
+    int ads_count = 0;
     for (int i = 0; i < n; i++) {
-        std::cout << A[i] << " ";
+        if (A[i].ads_count == 0) {
+            show(A, n, i, A[i].exit - 1);
+            ads_count++;
+        }
+        if (A[i].ads_count == 1) {
+            show(A, n, i, A[i].exit);
+            ads_count++;
+        }
     }
-    delete[] A;
+    std::cout << ads_count;
     return 0;
 }
