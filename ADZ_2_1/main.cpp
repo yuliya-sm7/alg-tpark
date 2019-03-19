5be9f4209daeca740d10b259061e4c3de6572763
yu <yuliya.sm7@gmail.com>
2019-03-20 00:33:19 +0300

    Optimize OK

--- ADZ_2_1/main.cpp
+++ ADZ_2_1/main.cpp
@@ -10,23 +10,25 @@
  */
 
 #include <iostream>
+#include <stdarg.h>
 
-bool input_arr(int *&A, int *&B, size_t &n, size_t &m) {
-    std::cin >> n >> m;
-    if (n == 0 || m == 0)
+
+bool input_arr(int *&A, size_t &n) {
+    if (n < 1)
         return false;
     A = new int[n];
     for (size_t i = 0; i < n; ++i)
         std::cin >> A[i];
-    B = new int[m];
-    for (size_t i = 0; i < m; ++i)
-        std::cin >> B[i];
     return true;
 }
 
-void delete_input_arr(int A[], int B[]) {
-    delete[] A;
-    delete[] B;
+void delete_arr(int num, ...) {
+    va_list ap;
+    va_start(ap, num);
+    while (num--) {
+        delete[] va_arg(ap, int*);
+    }
+    va_end(ap);
 }
 
 
@@ -47,33 +49,36 @@ ssize_t binary_check(int element, const int *array, ssize_t begin, ssize_t end) 
 
 
 // Поиск первого элемента массива array, который больше или равен element
-size_t greater_or_equal(int elem[], int *array, size_t size) {
+int *greater_or_equal(const int A[], const int B[], size_t n, size_t m) {
+    int *answer = (int *) calloc(m, sizeof(int));
     for (size_t i = 0; i < m; ++i) {
         ssize_t begin = -1;
-        ssize_t end = 0;
+        ssize_t end = 1;
         ssize_t k = -1;
-        while (k < 0 && end < size) {
+        while (k < 0 && end < n) {
             end = (begin + 1) * 2;
-            if (end < size && array[end] >= elem)
-                k = binary_check(elem, array, begin, end);
-            else if (array[end] >= elem)
-                k = binary_check(elem, array, begin, size - 1);
+            if (end < n && A[end] >= B[i])
+                k = binary_check(B[i], A, begin, end);
+            else
+                k = binary_check(B[i], A, begin, n - 1);
             begin = end;
         }
-        return k != -1 ? k : size;
+        answer[i] = (k != -1) ? (int) k : (int) n;
     }
+    return answer;
 }
 
 
 int main() {
     int *A, *B;
     size_t n, m;
-    if (input_arr(A, B, n, m)) {
-        size_t *answer = greater_or_equal(B, A, n);
+    std::cin >> n >> m;
+    input_arr(A, n);
+    input_arr(B, m);
+    int *answer = greater_or_equal(A, B, n, m);
 
-        for (size_t i = 0; i < m; ++i)
-            std::cout << greater_or_equal(B[i], A, n) << " ";
-        delete_input_arr(A, B);
-    }
+    for (size_t i = 0; i < m; ++i)
+        std::cout << answer[i] << " ";
+    delete_arr(3, A, B, answer);
     return 0;
 }
